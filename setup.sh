70ebcbc46a4cacefeca4b549fce271d123e68c65
Syrus Akbary <dimension.net@gmail.com>
2010-05-10 04:52:01 +0200

    Changed setup file

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir FuzzyUppaal;
+cd FuzzyUppaal;
+git clone git://github.com/SyrusAkbary/FuzzyUppaal-Java.git .;
+mkdir bin;
+cd lib;
+architecture=`uname -m`
+if [ "$architecture" != "x86_64" ] && [ "$architecture" != "ia64" ]; then
+    url="http://github.com/downloads/SyrusAkbary/RobotLand-Java/swt32.jar"
+else
+    url="http://github.com/downloads/SyrusAkbary/RobotLand-Java/swt64.jar"
+fi
+wget $url -O swt.jar;
+cd ..;
+echo "javac -Xlint -sourcepath src/ -classpath lib/swt.jar -d bin/ src/View.java 
+java -classpath bin/:lib/swt.jar View" >> run.sh;
+chmod +x run.sh;
